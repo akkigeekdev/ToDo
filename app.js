fc1689c4063bf83fb63dd1c07e17d093ee75c7a0
akkigeekdev <akkigeek@AkkiGeek.local>
2018-07-29 14:27:17 +0530

    UI fixed

--- app.js
+++ app.js
@@ -1,97 +1,102 @@
 let todo = (function(d, ls){
 
+    // todo list template
+    // { text: "", checked: false/true, id:random }
+
+
     // get content container
     let container = d.querySelector("#todocontent");
 
-    // get textbox
-    let txt = d.querySelector("#todotxt");
 
-
-    // reload saved todos
-    let savedTodo = JSON.parse(ls.getItem("todos")) || []; // change itemname accordingl to login name or login username /ID 
-    if(savedTodo.length > 0){
-        loadTodos();
+    // all the list will be saved here - Array
+    let savedLists = JSON.parse(ls.getItem("todos")) || []; // change itemname accordingl to login name or login username /ID 
+    if(savedLists.length > 0){
+        for (let i = 0; i < savedLists.length; i++) add(savedLists[i]);
     }
 
-    
 
-    // bind enter key in textbox
-    txt.addEventListener("keydown", function(evt){
-        console.log(evt , "got event");
+    // this function add the list in container 
+    function add(list){
 
-        // if enter - add and clear
-        if(evt.keyCode===13){
-            evt.preventDefault();
-            if(txt.value.trim() != ""){
+        // create Node
+        let template = '<div> <label data-id="' + list.id +'"></label> </div>'
+        +'<div>' + list.text + '</div>'
+        +'<div> <img data-id="' + list.id +'" src="delete.svg"> </div>';
 
-                let obj = {
-                    text : txt.value.trim(),
-                    checked : false,
-                    id : Math.random()
-                };
-
-                add(obj);
-                
-
-                //store it
-                savedTodo.push(obj);
-                ls.setItem("todos", JSON.stringify(savedTodo));
-
-                txt.value = "";
-            }
-        }
-
-    });
-
-    function loadTodos(){
-        for (let i = 0; i < savedTodo.length; i++) {
-            const todo = savedTodo[i];
-            add(todo);
-        }
-    }
-
-    function add(o){
         let listdiv = d.createElement("div");
         listdiv.classList.add("list");
-        listdiv.appendChild(chkBoxDiv());
-        listdiv.appendChild(contentDiv(o.text));
-        listdiv.appendChild(deleteDiv(o.id));
+        if(list.checked) listdiv.classList.add("checked");
+        listdiv.innerHTML = template;
+
+        //add to main container
         container.appendChild(listdiv);
+
+        // add delete event
+        listdiv.querySelector("img").addEventListener("click", remove);
+        
+        //add check event
+        listdiv.querySelector("label").addEventListener("click", checkHandler);
+
+
+    }
+    function save(list){
+        savedLists.push(list);
+        localStorage.setItem("todos", JSON.stringify(savedLists));
     }
 
-    function remove(nodeid){}
+    function remove(e){
+        
+        // remove node
+        let listdiv = e.target.closest(".list");
+        listdiv.parentNode.removeChild(listdiv);
 
-    function chkBoxDiv(){
-        let div = d.createElement("div");
+        //remove from localstorage
+        let removeindex = savedLists.findIndex(function(list){ return list.id == e.target.dataset.id});
+        savedLists.splice(removeindex,1);
 
-        let chkbox = d.createElement("input");
-        chkbox.type = "checkbox"; chkbox.id = "chk_"+savedTodo.length;
-
-        let lbl = d.createElement("label");
-        lbl.for = chkbox.id;
-
-        div.appendChild(chkbox); div.appendChild(lbl);
-
-        return div;
+        localStorage.setItem("todos", JSON.stringify(savedLists));
     }
 
-    function contentDiv(txt){
-        let div = d.createElement("div");
-        div.innerHTML = txt;
-        return div;
+    function checkHandler(e){
+
+        let chkbox = e.target;
+
+        let listIndex = savedLists.findIndex(function(l){return l.id == chkbox.dataset.id});
+
+        if(savedLists[listIndex].checked){
+            e.target.closest(".list").classList.remove("checked");
+            savedLists[listIndex].checked = false;
+        }
+        else{
+            e.target.closest(".list").classList.add("checked");
+            savedLists[listIndex].checked = true;
+        }
+
+        localStorage.setItem("todos", JSON.stringify(savedLists));
     }
 
-    function deleteDiv(id){
-        let div = d.createElement("div");
-        let img = d.createElement("img");
-        img.src = "delete.svg";
-        img.dataset.Delete = id;
-        div.appendChild(img);
-        return div;
-    }
 
     return{
-        add : add,
-        remove: remove
+        save: save,
+        add : add
     };
 })(document, localStorage);
+
+
+// bind enter key in textbox
+document.querySelector("#todotxt").addEventListener("keydown", function(evt){
+
+    let txt = evt.target;
+
+    // if enter - add and clear
+    if(evt.keyCode===13){
+        evt.preventDefault();
+        if(txt.value.trim() != ""){
+            let listDetail = {text: txt.value, checked:false, id: (Math.floor(1000 + Math.random() * 9000))};
+            todo.add(listDetail);
+            todo.save(listDetail);
+            txt.value = "";
+        }
+    }
+
+});
